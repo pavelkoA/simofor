5474e4e271ceb0e531cda551661e48578c099fc2
pavelkoA <115475612+pavelkoA@users.noreply.github.com>
2023-01-10 22:43:05 +0300

    main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import ClassesAndMethods
+import time
+import ClassesDevice
+from datetime import *
 
 if __name__ == '__main__':
-    HP_serve = ClassesAndMethods.Device('belomor', '192.168.0.241', 161)
-    print(HP_serve.snmp_get_next('1.3.6.1.2.1.1.5.0'))
+    HP_swich_srv = ClassesDevice.Device('belomor', '192.168.0.241', 161)
+    print(HP_swich_srv.snmp_get_next('1.3.6.1.2.1.1.5.0'))
+    time_up = int(HP_swich_srv.snmp_get_next('1.3.6.1.2.1.1.3.0'))
+    print(timedelta(seconds=time_up))
+    print(HP_swich_srv.snmp_get_next('1.3.6.1.2.1.1.1.0'))
+    print(HP_swich_srv.snmp_get_next('1.3.6.1.4.1.25506.2.6.1.1.1.1.12'))
+    # print(HP_swich_srv.snmp_get_next('1.3.6.1.2.1.1.3.0'))
+    # print(HP_swich_srv.snmp_get_next('1.3.6.1.2.1.1.3.0'))
 
 
